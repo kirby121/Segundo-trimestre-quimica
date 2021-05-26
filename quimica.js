ec05e1f4a5f9a2b260d6bf2170521211a03fc813
kirby121 <83082132+kirby121@users.noreply.github.com>
2021-05-26 12:50:19 -0500

    Update quimica.js

--- quimica.js
+++ quimica.js
@@ -1,1 +1,20 @@
-if (window.matchMedia("(max-width: 1024px)").matches) alert("Este contenido se ve mejor en pantallas un poco más grandes (por ejemplo computadores)");
+if (window.matchMedia("(max-width: 1024px)").matches) console.log();
+
+function hamburguerMenu(panelBtn, panel, menuLink) {
+
+  const d = document;
+
+  d.addEventListener("click", e => {
+    if (e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)) {
+      d.querySelector(panel).classList.toggle("is-active");
+      d.querySelector(panelBtn).classList.toggle("is-active");
+    }
+    if (e.target.matches(menuLink)) {
+      d.querySelector(panel).classList.remove("is-active");
+      d.querySelector(panelBtn).classList.remove("is-active");
+
+    }
+  })
+}
+
+hamburguerMenu(".panel-btn", ".panel", ".menu a");
